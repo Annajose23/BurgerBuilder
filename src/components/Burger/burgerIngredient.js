cdb468e7c1d4676ecc97b3bc8098715ba6458976
Anna Jose <joann23july@gmail.com>
2020-07-27 18:26:06 -0400

    updated

---
+++ src/components/Burger/burgerIngredient.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from '../Burger/'
+
+const BurgerIngredient = (props) => {
+        let ingredient = null;
+
+        switch(props.type){
+            case('bread-bottom'):
+            ingredient = <div className={} 
+        }
+
+    
+}
+ 
+export default BurgerIngredient;
